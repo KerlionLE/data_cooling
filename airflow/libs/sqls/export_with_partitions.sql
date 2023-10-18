09e8e22c9242c1dfc95fba72442dcdc8796bc7ce
Максим <maksim@MacBook-Pro-Anton.local>
2023-10-18 10:09:56 +0300

    Update files

--- airflow/libs/sqls/export_with_partitions.sql
+++ airflow/libs/sqls/export_with_partitions.sql
@@ -1,5 +1,5 @@
 -- Выгружаем данные из вертики в hdfs с партицированием "на лету" (parquet)
-EXPORT TO PARQUET(directory='webhdfs:///data/vertica/{schema_name}/{table_name}TT_{current_date}', compression='snappy') OVER(PARTITION BY part) AS 
+EXPORT TO PARQUET(directory='webhdfs:///data/vertica/{schema_name}/{table_name}T_{current_date}', compression='snappy') OVER(PARTITION BY part) AS 
 SELECT *, {partition_expressions} as part 
 FROM {schema_name}.{table_name} 
 WHERE 1=1 {filter_expression} 
