30783f1dc9c4be641fdbbb201684e233e5bb1236
Максим <maksim@MacBook-Pro-Anton.local>
2023-10-18 13:57:30 +0300

    Update files

--- airflow/libs/vrt_to_hdfs.py
+++ airflow/libs/vrt_to_hdfs.py
@@ -19,7 +19,7 @@ def execute_sql(sql, conf_con_info):
 def get_last_date_cooling(conf_con_info, conf_query):
     sql = f'''
                     select schema_table_name, max(last_data_cooling)
-                    from devdb.sandbox.data_cooling
+                    from sandbox.data_cooling
                     where schema_table_name = {conf_query['schema_name']}.{conf_query['table_name']}
                     group by schema_table_name
                 '''
