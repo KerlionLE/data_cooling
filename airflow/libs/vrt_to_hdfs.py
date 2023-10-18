e1eeb8d347a920807cc82cba76366fd371e0422b
Максим <maksim@MacBook-Pro-Anton.local>
2023-10-18 18:52:05 +0300

    Update files

--- airflow/libs/vrt_to_hdfs.py
+++ airflow/libs/vrt_to_hdfs.py
@@ -67,13 +67,13 @@ def put_last_date_cooling(conf_con_info, conf_query, sql_scripts_path, new_last_
 
 def con_kerberus_vertica(conf_con_info, conf_krb_info, conf_query_info, sql_scripts_path):
     last_cooling_dates = {}
-    current_date = datetime.now().date()
-    current_date = current_date.strftime("%Y-%m-%d")
+    current_date_date = datetime.now().date()
+    current_date = current_date_date.strftime("%Y-%m-%d")
 
     with Kerberos(conf_krb_info['principal'], conf_krb_info['keytab']):
         for conf_query in conf_query_info:
             last_date_cooling = get_last_date_cooling(conf_con_info, conf_query, sql_scripts_path)
-            if (current_date - datetime.strptime(last_date_cooling[1],'%Y_%m_%d')).days == conf_query['data_cooling_frequency']:      
+            if (current_date_date - datetime.strptime(last_date_cooling[1],'%Y_%m_%d')).days == conf_query['data_cooling_frequency']:      
                 if not conf_query['partition_expressions']:
 
                     sql = get_formated_file(
