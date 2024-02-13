c70fff0520bcb3ca8f21278119f026327324608b
Максим <maksim@MacBook-Pro-Anton.local>
2024-02-13 12:44:43 +0300

    TEST

--- airflow/libs/sqls/copy_to_vertica.sql
+++ airflow/libs/sqls/copy_to_vertica.sql
@@ -1,2 +1,2 @@
 -- Загружаем данные назад в Vertica
-COPY {schema_name}.{table_name} FROM 'webhdfs:///data/vertica/{schema_name}/{table_name}/tech_hdfs_load_ts=20240213/part=2023-09-12/*.parquet' PARQUET;
+COPY {schema_name}.{table_name} FROM 'webhdfs:///data/vertica/{schema_name}/{table_name}/tech_hdfs_load_ts=20240213/part=2023-09-12/d5719e0e-v_devdb_node0002-140210318694144-0.parquet' PARQUET(do_soft_schema_match_by_name='True');
--- airflow/libs/sqls/export_with_partitions.sql
+++ airflow/libs/sqls/export_with_partitions.sql
@@ -1,5 +1,5 @@
 -- Выгружаем данные из вертики в hdfs с партицированием "на лету" (parquet)
-EXPORT TO PARQUET(directory='webhdfs:///data/vertica/{schema_name}/{table_name}/', compression='snappy') OVER(PARTITION BY part) AS 
+EXPORT TO PARQUET(directory='webhdfs:///data/vertica/{schema_name}/{table_name}/tech_hdfs_load_ts={cur_date}', compression='snappy') OVER(PARTITION BY part) AS 
 SELECT *, {partition_expressions} as part 
 FROM {schema_name}.{table_name} 
 WHERE 1=1 {filter_expression} {time_between}
