93a9d0bb82478f31799ca58df7ee7adb1a66395c
Максим <maksim@MacBook-Pro-Anton.local>
2024-02-19 16:20:44 +0300

    TEST

--- airflow/libs/config_manager/datacatalog_conf_manager.py
+++ airflow/libs/config_manager/datacatalog_conf_manager.py
@@ -107,7 +107,7 @@ class DataCatalogConfManager(ConfigManager):
         
         for a in id_objs_cool_parms: 
             for b in data_list_cool_results:
-                if int(a['id']) == int(b['physicalObjectCoolParamsId']):
+                if a['id'] == b['physicalObjectCoolParamsId']:
                    a['coolingLastDate'] = b['coolingLastDate']
                    a['coolingHdfsTarget'] = b['coolingHdfsTarget']
 
