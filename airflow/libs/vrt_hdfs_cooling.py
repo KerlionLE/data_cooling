79f609ede70e7b9e5604f24680113452a902c86d
Максим <maksim@MacBook-Pro-Anton.local>
2024-02-22 15:28:01 +0300

    TEST

--- airflow/libs/vrt_hdfs_cooling.py
+++ airflow/libs/vrt_hdfs_cooling.py
@@ -26,11 +26,12 @@ def get_last_tech_load_ts(schemas: list, tables: list, schema_table_name_registr
         sql_scripts_path,
         schema_table_name=schema_table_name_registry,
         schema_names=', '.join(f'{element!r}' for element in schemas),
-        table_names=', '.join(f'{element!r}' for element in schemas),
+        table_names=', '.join(f'{element!r}' for element in tables),
     )
-    return {
-        (schema_name, table_name): {'tech_load_ts': tech_load_ts}
-        for schema_name, table_name, tech_load_ts in db_connection_src.apply_script_hdfs(sql, conf_krb_info)[0]}
+
+    obj = {(schema_name, table_name): {'tech_load_ts': tech_load_ts} for schema_name, table_name, tech_load_ts in db_connection_src.apply_script_hdfs(sql, conf_krb_info)[0]}
+    
+    return obj
 
 # ------------------------------------------------------------------------------------------------------------------
 
