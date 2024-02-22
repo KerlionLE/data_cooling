22160031d274f5be0e4fc143bf3c1b91944eb4fe
Максим <maksim@MacBook-Pro-Anton.local>
2024-02-22 15:01:41 +0300

    TEST

--- airflow/libs/vrt_hdfs_cooling.py
+++ airflow/libs/vrt_hdfs_cooling.py
@@ -146,11 +146,11 @@ def gen_dml(config: list, copy_to_vertica: str, delete_with_partitions: str, exp
 
         date_start = conf.get('last_date_cooling') or '1000-10-01 15:14:15'
         partition = conf.get('partition_expressions') or f'DATE({tech_ts_column_name})'
-        date_type = '%Y-%m-%d %H:%M:%S'
+        date_format = '%Y-%m-%d %H:%M:%S'
 
         current_date = datetime.now()
         date_end_cooling_depth = (datetime.strptime(
-            actual_max_tech_load_ts, date_type) - timedelta(days=depth_cooling)).strftime(date_type)
+            actual_max_tech_load_ts, date_format) - timedelta(days=depth_cooling)).strftime(date_format)
 
         sql_export_date_start_date_end_cooling_depth = get_formated_file(
             export_with_partitions,
@@ -175,9 +175,9 @@ def gen_dml(config: list, copy_to_vertica: str, delete_with_partitions: str, exp
                 if temporary_heating:
 
                     depth_heating = conf['temporary_heating']['depth']
-                    date_end_heating_depth = (datetime.strptime(actual_max_tech_load_ts, date_type) - timedelta(days=depth_heating)).strftime(date_type)
-                    date_end_heating = datetime.strptime(conf['temporary_heating']['date_end'], date_type)
-                    date_start_heating = datetime.strptime(conf['temporary_heating']['date_start'], date_type)
+                    date_end_heating_depth = (datetime.strptime(actual_max_tech_load_ts, date_format) - timedelta(days=depth_heating)).strftime(date_format)
+                    date_end_heating = datetime.strptime(conf['temporary_heating']['date_end'], date_format)
+                    date_start_heating = datetime.strptime(conf['temporary_heating']['date_start'], date_format)
 
                     sql_delete_date_start_date_end_heating_depth = get_formated_file(
                         delete_with_partitions,
