b6e6df384905dcaa91afb02587d7cc26d36f037a
Максим <maksim@MacBook-Pro-Anton.local>
2024-02-22 09:17:53 +0300

    TEST

--- airflow/libs/config_manager/datacatalog_conf_manager.py
+++ airflow/libs/config_manager/datacatalog_conf_manager.py
@@ -6,7 +6,6 @@ from pydg.data_catalog.model.dicts import DataCatalogEntityType
 
 from .conf_manager import ConfigManager
 
-
 def type_to_dict(obj: str) -> str:
     """
     Функция реализована для работы со структурой обект в объекте
@@ -31,6 +30,122 @@ def params_to_dict(obj: str) -> dict:
         d[name] = value if name != 'coolingType' or name != 'heatingType' else type_to_dict(value)
     return d
 
+def PhysicalObjectCoolParams_PhysicalObjectCoolResult(repo: str) -> list:
+    """
+    Обработка конфига охлаждения - json формата из data catalog
+    :param repo: сессия con
+
+    :return data_list_cool: лист с обектами для охлаждения
+    :return id_objs_cool_parms: лист только с id для фильтра к api
+    """
+   
+    request_cool_parms = {
+    'quer': {
+        'coolingIsActive': True,
+    },
+    'page': 1,
+    'pageSize': 300,
+    }
+
+    request_cool_results = {
+    'query': {
+        'physicalObjectCoolParamsId': 39,
+    },
+    'page': 1,
+    'pageSize': 300,
+    }
+
+    # 1 Работа с обектом PhysicalObjectCoolParams
+    get_cool_parms = repo.readEntity(
+        entityType=DataCatalogEntityType.PhysicalObjectCoolParams.value,
+        payload=request_cool_parms,
+    )
+
+    data_list_cool_parms = []
+    for d in get_cool_parms['items']:
+        data_list_cool_parms.append(params_to_dict(d))
+
+    # 1.1.1 Берём список таблиц для того не тащить имена таблиц
+    id_objs_cool_parms = [d.get('physicalObjectId') for d in data_list_cool_parms]
+
+    # 1.1 Работа с обектом PhysicalObjectCoolResult
+    get_cool_results = repo.readEntity(
+        entityType=DataCatalogEntityType.PhysicalObjectCoolResult.value,
+        payload=request_cool_results,
+    )
+    data_list_cool_results = []
+    for d in get_cool_results['items']:
+        data_list_cool_results.append(params_to_dict(d))
+
+    # 1.2 Объединение PhysicalObjectCoolParams и PhysicalObjectCoolResult
+    data_list_cool = []
+    for a in data_list_cool_parms:
+        if len(data_list_cool_results) != 0:
+            for b in data_list_cool_results:
+                if a['id'] == b['physicalObjectCoolParamsId']:
+                    a['coolingLastDate'] = b['coolingLastDate']
+                    a['coolingHdfsTarget'] = b['coolingHdfsTarget']
+                    data_list_cool.append(a)
+        else:
+            data_list_cool.append(a)
+    
+    return data_list_cool, id_objs_cool_parms
+
+def PhysicalObjectHeatParams_PhysicalObjectHeatResult(repo: str) -> list:
+    """
+    Обработка конфига разогрева - json формата из data catalog
+    :param repo: сессия con
+
+    :return: лист
+    """
+
+    request_heating_parms = {
+    'query': {
+        'heatingIsActive': True,
+    },
+    'page': 1,
+    'pageSize': 300,
+    }
+
+    request_heating_results = {
+    'query': {
+        'physicalObjectHeatParamsId': 39,
+    },
+    'page': 1,
+    'pageSize': 300,
+    }
+
+    # 2 Работа с обектом PhysicalObjectHeatParams
+    get_heat_param = repo.readEntity(
+        entityType=DataCatalogEntityType.PhysicalObjectHeatParams.value,
+        payload=request_heating_parms,
+    )
+
+    data_list_heat_parms = []
+    for d in get_heat_param['items']:
+        data_list_heat_parms.append(params_to_dict(d))
+
+    # 2.1 Работа с обектом PhysicalObjectHeatResult
+    get_heat_results = repo.readEntity(
+        entityType=DataCatalogEntityType.PhysicalObjectHeatResult.value,
+        payload=request_heating_results,
+    )
+
+    data_list_heat_results = []
+    for d in get_heat_results['items']:
+        data_list_heat_results.append(params_to_dict(d))
+
+    # 2.2 Объединение PhysicalObjectHeatParams и PhysicalObjectHeatResult
+    data_list_heat = []
+    for a in data_list_heat_parms:
+        if len(data_list_heat_results) != 0:
+            for b in data_list_heat_results:
+                if a['id'] == b['physicalObjectHeatParamsId']:
+                    a['heatingExternalTableName'] = b['heatingExternalTableName']
+                    a['isAlreadyHeating'] = b['isAlreadyHeating']
+                    data_list_heat.append(a)
+        else:
+            data_list_heat.append(a)
 
 class DataCatalogConfManager(ConfigManager):
     """Класс Обработки конфига - включает в себя get и save"""
@@ -40,7 +155,6 @@ class DataCatalogConfManager(ConfigManager):
         Обработка конфига - json формата из data catalog
         :param conf: возможные параметры конфига
 
-        :return: лист внутри json
         """
         base_url = 'https://dg.dev002.local/dc-blue'  # URL прода, теста или дева
         root_ca_path = self.config['root_ca_path']
@@ -63,103 +177,8 @@ class DataCatalogConfManager(ConfigManager):
         repo = Repo(session, logger)
         logger.info('Execute query')
 
-        request_cool_parms = {
-            'quer': {
-                'coolingIsActive': True,
-            },
-            'page': 1,
-            'pageSize': 300,
-        }
-
-        request_cool_results = {
-            'query': {
-                'physicalObjectCoolParamsId': 39,
-            },
-            'page': 1,
-            'pageSize': 300,
-        }
-
-        request_heating_parms = {
-            'query': {
-                'heatingIsActive': True,
-            },
-            'page': 1,
-            'pageSize': 300,
-        }
-
-        request_heating_results = {
-            'query': {
-                'physicalObjectHeatParamsId': 39,
-            },
-            'page': 1,
-            'pageSize': 300,
-        }
-
-        # 1 Работа с обектом PhysicalObjectCoolParams
-        get_cool_parms = repo.readEntity(
-            entityType=DataCatalogEntityType.PhysicalObjectCoolParams.value,
-            payload=request_cool_parms,
-        )
-
-        data_list_cool_parms = []
-        for d in get_cool_parms['items']:
-            data_list_cool_parms.append(params_to_dict(d))
-
-        # 1.1.1 Берём список таблиц для того не тащить имена таблиц
-        id_objs_cool_parms = [d.get('physicalObjectId') for d in data_list_cool_parms]
-
-        # 1.1 Работа с обектом PhysicalObjectCoolResult
-        get_cool_results = repo.readEntity(
-            entityType=DataCatalogEntityType.PhysicalObjectCoolResult.value,
-            payload=request_cool_results,
-        )
-        data_list_cool_results = []
-        for d in get_cool_results['items']:
-            data_list_cool_results.append(params_to_dict(d))
-
-        # 1.2 Объединение PhysicalObjectCoolParams и PhysicalObjectCoolResult
-        data_list_cool = []
-        for a in data_list_cool_parms:
-            if len(data_list_cool_results) != 0:
-                for b in data_list_cool_results:
-                    if a['id'] == b['physicalObjectCoolParamsId']:
-                        a['coolingLastDate'] = b['coolingLastDate']
-                        a['coolingHdfsTarget'] = b['coolingHdfsTarget']
-                        data_list_cool.append(a)
-            else:
-                data_list_cool.append(a)
-
-        # 2 Работа с обектом PhysicalObjectHeatParams
-        get_heat_param = repo.readEntity(
-            entityType=DataCatalogEntityType.PhysicalObjectHeatParams.value,
-            payload=request_heating_parms,
-        )
-
-        data_list_heat_parms = []
-        for d in get_heat_param['items']:
-            data_list_heat_parms.append(params_to_dict(d))
-
-        # 2.1 Работа с обектом PhysicalObjectHeatResult
-        get_heat_results = repo.readEntity(
-            entityType=DataCatalogEntityType.PhysicalObjectHeatResult.value,
-            payload=request_heating_results,
-        )
-
-        data_list_heat_results = []
-        for d in get_heat_results['items']:
-            data_list_heat_results.append(params_to_dict(d))
-
-        # 2.2 Объединение PhysicalObjectHeatParams и PhysicalObjectHeatResult
-        data_list_heat = []
-        for a in data_list_heat_parms:
-            if len(data_list_heat_results) != 0:
-                for b in data_list_heat_results:
-                    if a['id'] == b['physicalObjectHeatParamsId']:
-                        a['heatingExternalTableName'] = b['heatingExternalTableName']
-                        a['isAlreadyHeating'] = b['isAlreadyHeating']
-                        data_list_heat.append(a)
-            else:
-                data_list_heat.append(a)
+        data_list_cool, id_objs_cool_parms = PhysicalObjectCoolParams_PhysicalObjectCoolResult(repo)
+        data_list_heat = PhysicalObjectHeatParams_PhysicalObjectHeatResult(repo)
 
         # 3 Объединение Heat и Cool
         data_list = []
@@ -232,5 +251,3 @@ class DataCatalogConfManager(ConfigManager):
                     a['physicalName'] = b['physicalName']
                     a['physicalNameGroup'] = b['physicalNameGroup']
                     data_list_all.append(a)
-
-        return get_cool_parms, get_cool_results
