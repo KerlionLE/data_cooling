1a0f22926a95f34a25b8c52f4e251423fbe719d3
Максим <maksim@MacBook-Pro-Anton.local>
2024-03-12 09:00:02 +0300

    TEST

--- airflow/libs/config_manager/datacatalog_conf_manager.py
+++ airflow/libs/config_manager/datacatalog_conf_manager.py
@@ -164,8 +164,8 @@ def compound_heat_cool(data_list_cool: list, data_list_heat: list) -> list:
     for a in data_list_cool:
         for b in data_list_heat:
             if a['physicalObjectId'] == b['physicalObjectId']:
-                print(a)
-                print(b)
+                a = frozenset(a)
+                b = frozenset(b)
                 a[{temporary_heating}]['heatingType'] = b['heatingType']
                 a[{temporary_heating}]['heatingDepthDays'] = b['heatingDepthDays']
                 a[{temporary_heating}]['heatingStartDate'] = b['heatingStartDate']
