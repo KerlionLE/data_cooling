e963e3661a0b066261f49816ca3b3b7424876a8c
Максим <maksim@MacBook-Pro-Anton.local>
2024-03-14 10:57:46 +0300

    TEST

--- airflow/libs/vrt_hdfs_cooling.py
+++ airflow/libs/vrt_hdfs_cooling.py
@@ -146,8 +146,8 @@ def gen_dml(config: list, copy_to_vertica: str, delete_with_partitions: str, exp
                     depth_heating = conf['heating_depth']
                     date_end_heating_depth = (datetime.strptime(actual_max_tech_load_ts, date_format) - timedelta(days=int(depth_heating))).strftime(date_format)
                     print(f'''1 - {conf['heating_date_end']}, 2 - {conf['heating_date_start']}''')
-                    date_end_heating = datetime.strptime(conf['heating_date_end'].replace(tzinfo=None), date_format)
-                    date_start_heating = datetime.strptime(conf['heating_date_start'].replace(tzinfo=None), date_format)
+                    date_end_heating = datetime.strptime(conf['heating_date_end'], date_format)
+                    date_start_heating = datetime.strptime(conf['heating_date_start'], date_format)
 
                     sql_delete_date_start_date_end_heating_depth = get_formated_file(
                         delete_with_partitions,
