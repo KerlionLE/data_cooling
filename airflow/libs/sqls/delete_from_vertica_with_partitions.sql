e226e88833a363db62b71ef60f483f2901243674
Максим <maksim@MacBook-Pro-Anton.local>
2024-03-29 14:01:13 +0300

    TEST

--- airflow/libs/sqls/delete_from_vertica_with_partitions.sql
+++ airflow/libs/sqls/delete_from_vertica_with_partitions.sql
@@ -1,2 +1,14 @@
-DELETE FROM {schema_name}.{table_name} 
-WHERE  1=1 {filter_expression} {time_between};
+DROP TABLE IF EXISTS {schema_name}.{table_name}{postfix};
+
+CREATE TABLE {schema_name}.{table_name}{postfix} LIKE {schema_name}.{table_name} INCLUDING PROJECTIONS;
+
+INSERT INTO {schema_name}.{table_name}{postfix}
+SELECT *
+FROM {schema_name}.{table_name}
+WHERE 1=1 {time_between};
+
+TRUNCATE TABLE {schema_name}.{table_name};
+
+SELECT copy_table('{schema_name}.{table_name}{postfix}','{schema_name}.{table_name}');
+
+DROP TABLE {schema_name}.{table_name}{postfix}
